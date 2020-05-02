85e24a8fac4466f211f569b411901be90bca40e2
Miguel Zambrano <ctcdesarrollo5@hotmail.com>
2020-05-02 18:27:23 -0500

    se agrega alerta dialogo

--- app/src/main/java/com/main/airbnb/FormActivity.java
+++ app/src/main/java/com/main/airbnb/FormActivity.java
@@ -1,8 +1,13 @@
 package com.main.airbnb;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -13,5 +18,34 @@ public class FormActivity extends AppCompatActivity {
 
         // Ocultar el actionBar
         getSupportActionBar().hide();
+    }
+
+    public void showDialog(View view) {
+        AlertDialog.Builder myAlertBuilder = new AlertDialog.Builder(this);
+        myAlertBuilder.setTitle("Alerta");
+        myAlertBuilder.setMessage(R.string.text_dialog);
+        myAlertBuilder.setPositiveButton("OK", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User clicked OK button.
+                        Toast.makeText(getApplicationContext(), R.string.text_ok_dialog,
+                                Toast.LENGTH_SHORT).show();
+                        IniciarSession();
+                    }
+                });
+        myAlertBuilder.setNegativeButton("Cancel", new
+                DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog.
+                        Toast.makeText(getApplicationContext(), R.string.text_info,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        myAlertBuilder.show();
+    }
+
+    public  void IniciarSession(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
